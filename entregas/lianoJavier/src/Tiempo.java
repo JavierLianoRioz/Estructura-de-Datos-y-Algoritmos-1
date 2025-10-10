423c9d70ae7d704e245ec93935bbe9606016585b
midas@uneat <javier.liano@alumnos.uneatlantico.es>
2025-10-10 13:06:34 +0200

    fix: error en el metodo menosOIgualDe()

--- entregas/lianoJavier/src/Tiempo.java
+++ entregas/lianoJavier/src/Tiempo.java
@@ -4,7 +4,7 @@ public class Tiempo {
     private int minutos;
 
     public boolean menosOIgualDe(int minutos) {
-        return minutos <= 10;
+        return this.minutos <= minutos;
     }
 
     public void siguiente() {
