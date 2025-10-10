17fd696ce6314d799071d2eb3a04805489a1b861
midas@uneat <javier.liano@alumnos.uneatlantico.es>
2025-10-10 13:54:40 +0200

    feat: implementar niño

--- entregas/lianoJavier/src/Niño.java
+++ entregas/lianoJavier/src/Niño.java
@@ -1,28 +1,75 @@
 
 public class Niño {
 
+    private String nombre;
+    private int edad;
+
+    private Niño siguienteNiño;
+
+    private String pizarrin;
+    private boolean haJugado;
+
+    private static final String ABECEDARIO = "abcdefghijklmnopqrstuvwxyz";
+
     public Niño(String nombre, int edad) {
-        //TODO Auto-generated constructor stub
+        this.nombre = nombre;
+        this.edad = edad;
     }
 
-    public void lee(String pizarrin) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'recibe'");
+    public void lee(String mensaje) {
+        anotar(mensaje);
+    }
+
+    public void anotar(String mensaje) {
+        pizarrin = alterar(mensaje);
+    }
+
+    private String alterar(String mensaje) {
+        char[] caracteres = mensaje.toCharArray();
+        int errores = 1 + azar(2);
+
+        for (int i = 0; i < errores; i++) {
+            int posicion = azar(caracteres.length);
+            char original = caracteres[posicion];
+            char reemplazo;
+
+            do {
+                reemplazo = ABECEDARIO.charAt(azar(ABECEDARIO.length()));
+            } while (Character.toLowerCase(original) == reemplazo);
+
+            caracteres[posicion] = Character.isUpperCase(original)
+                    ? Character.toUpperCase(reemplazo)
+                    : reemplazo;
+        }
+        return new String(caracteres);
+    }
+
+    private int azar(int limite) {
+        return (int) (Math.random() * limite);
     }
 
     public void limpiarPizarrin() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'limpiarPizarrin'");
+        pizarrin = "";
+        siguienteNiño.limpiarPizarrin();
     }
 
     public void jugar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'jugar'");
+        if (!haJugado) {
+            enseñar(pizarrin, siguienteNiño);
+            haJugado = true;
+        } else
+            siguienteNiño.jugar();
+    }
+
+    private void enseñar(String pizarrin, Niño niño) {
+        niño.lee(pizarrin);
     }
 
     public void recibe(Niño niño) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'recibe'");
+        if (siguienteNiño == null)
+            siguienteNiño = niño;
+        else
+            siguienteNiño.recibe(niño);
     }
 
 }
